0b992e3380dabac84eb59d5c9bf74a1b0ea4bc21
Isaac <isaaclkh9909@gmail.com>
2020-07-10 22:55:34 +0900

    BaekJun 1932 2020.07.10

---
+++ OneNineThreeTwo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void){
+    int n;
+    int *arr;
+
+    cin >> n;
+
+
+
+}
+
+https://m.blog.naver.com/PostView.nhn?blogId=occidere&logNo=220789773974&proxyReferer=https:%2F%2Fwww.google.com%2F
