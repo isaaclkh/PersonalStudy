1828fa327fc7547c2f5ff907ba261b8f58d80401
Guest <guest-br7ik1@h29853.sgb.skir.kr>
2020-07-12 17:10:45 +0900

    Not Yet But Almost

--- Stack&Que/first.cpp
+++ Stack&Que/first.cpp
@@ -1,22 +1,48 @@
-#include <string>
+/*#include <string>
 #include <vector>
 
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
-    int front;
+    int front = 0;
     
     while(!(truck_weights.empty())){
-        front = truck_weights.front();
+        front += truck_weights.front();
         truck_weights.pop_back();
         
-        while(front + truck_weights.front() <= weight){
+        if(front + truck_weights.front() <= weight){
             truck_weights.pop_back();
             answer += bridge_length;
         }
         
         answer += bridge_length;
+    }
+    
+    return answer;
+}*/
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = bridge_length;
+    int front = 0;
+    int size = truck_weights.size();
+    
+    while(size){
+        front = truck_weights.front();
+        truck_weights.pop_back();
+        
+        if(front + truck_weights.front() <= weight){
+            truck_weights.pop_back();
+            answer+=1;
+        }
+        
+        else answer += bridge_length;
+        size--;
     }
     
     return answer;
