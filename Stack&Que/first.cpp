ded02d9240f73ff7741f87f0a6d6c89bd20ce650
Isaac <isaaclkh9909@gmail.com>
2020-07-19 14:27:12 +0900

    Still has a problem

--- Stack&Que/first.cpp
+++ Stack&Que/first.cpp
@@ -20,9 +20,11 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
     }
     
     return answer;
-}*/
+} 
+case1 okay, case2 & case3 fail
+*/
 
-#include <string>
+/*#include <string>
 #include <vector>
 
 using namespace std;
@@ -38,7 +40,7 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
         
         if(front + truck_weights.front() <= weight){
             truck_weights.pop_back();
-            answer+=1;
+            answer++;
         }
         
         else answer += bridge_length;
@@ -47,3 +49,94 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
     
     return answer;
 }
+case1 fail, case2 & case3 okay
+*/
+
+/*
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = bridge_length;
+    int size = truck_weights.size();
+    int front;
+    int sum=0;
+    
+    vector<int> reversed;
+    
+    while (size){
+        reversed.push_back(truck_weights.back());
+        truck_weights.pop_back();
+        size--;
+    }
+    
+    size = reversed.size();
+    
+    while(size){
+        front = reversed.back();
+        sum += front;
+        reversed.pop_back();
+        
+        if(sum <= weight){
+            reversed.pop_back();
+            answer++;
+        }
+
+        else{
+            answer += bridge_length;
+            sum = 0;
+        }
+        
+        size--;
+    }
+    
+    return answer;
+}
+
+case1 & case2 okay, case3 fail
+*/
+
+/*
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    int answer = bridge_length;
+    int size = truck_weights.size();
+    int front;
+    int sum=0;
+    
+    vector<int> reversed;
+    
+    while (size){
+        reversed.push_back(truck_weights.back());
+        truck_weights.pop_back();
+        size--;
+    }
+    
+    size = reversed.size();
+    
+    while(size){
+        front = reversed.back();
+        sum += front;
+        reversed.pop_back();
+        
+        if(sum <= weight)  answer++;
+
+        else{
+            answer += bridge_length;
+            sum = 0;
+        }
+        
+        size--;
+    }
+    
+    return answer;
+}
+case1 & case2 & case3 okay but still has a problem
+-sum ==> has problem
+*/
