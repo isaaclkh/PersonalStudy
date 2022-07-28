aa0f2a6f4d8b0e71fed0444f93dc3c4a3bf32ab9
Isaac <isaaclkh9909@gmail.com>
2022-07-28 21:08:45 +0900

    Fifth (Futter Toast)

---
+++ fifth/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My app',
+      theme: ThemeData(primaryColor: Colors.grey),
+      home: const MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Toast message'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            flutterToast();
+          },
+          child: Text('Toast'),
+          style: TextButton.styleFrom(
+            primary: Colors.pink[200],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void flutterToast() {
+  Fluttertoast.showToast(
+    msg: 'Toast',
+    gravity: ToastGravity.BOTTOM,
+    backgroundColor: Colors.redAccent,
+    fontSize: 20.0,
+    textColor: Colors.white,
+    toastLength: Toast.LENGTH_SHORT,
+  );
+}
